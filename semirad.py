24f7484ad43b857d62e359fe702f82e8d7d178fc
Bryce Shaw <shaw.bryce@gmail.com>
2019-12-11 12:38:40 -0700

    very small change

--- semirad.py
+++ semirad.py
@@ -6,9 +6,10 @@ import re
 
 print("getting urls")
 
-url = 'https://semi-rad.com/category/friday-inspiration/'
 
 st = ""
+
+url = 'https://semi-rad.com/category/friday-inspiration/'
 req = requests.get(url, verify=False).text
 soup = BeautifulSoup(req, 'lxml')
 snip = soup.findAll('div', 'social-sharing')
@@ -16,8 +17,7 @@ for i in range(len(snip)):
     st += snip[i]['data-permalink'] + '\n'
 
 
-for page in [2, 3, 4, 5, 6, 7, 8]:
-
+for page in range(1,9):
     url = f"https://semi-rad.com/category/friday-inspiration/page/{page}/"
     req = requests.get(url, verify=False).text
     soup = BeautifulSoup(req, 'lxml')
@@ -26,9 +26,7 @@ for page in [2, 3, 4, 5, 6, 7, 8]:
         st += snip[i]['data-permalink'] + '\n'
 
 text_file = open("Friday-inspiration-urls.txt", "w")
-
 text_file.write(st)
-
 text_file.close()
 
 print('File write success. Wrote to \'Friday-inspiration-urls.txt\'')
